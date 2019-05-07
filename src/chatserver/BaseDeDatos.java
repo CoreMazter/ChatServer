7fb242f9eecf2657ae9b3354c6e1117a26cabbc4
XV02 <lghhssi@gmail.com>
2019-05-07 09:25:07 -0500

    Commit 07/05/2019 9:24
    - Arreglos menores en la interfaz y funcionalidades

---
+++ src/chatserver/BaseDeDatos.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatserver;
+
+import java.awt.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author lghhs
+ */
+public class BaseDeDatos
+{
+    private Connection con;
+    public BaseDeDatos()
+    {
+        try 
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/cocochat", "root", ""); 
+        } 
+        catch (ClassNotFoundException ex) 
+        {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Usuario selectUser(String nick)
+    {
+        ResultSet rs;
+        Usuario usuario = new Usuario(0, "", "");
+        try 
+        {
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM usuario WHERE nickname='"+nick+"'");    
+            rs = statement.executeQuery();
+            while(rs.next())
+            {
+                usuario.setId(rs.getInt("id_u"));
+                usuario.setNickname(rs.getString("nickname"));
+                usuario.setPassword(rs.getString("password"));
+            }
+            return usuario;
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return usuario;
+    }
+    
+    public ArrayList<Usuario> selectAllUsers()
+    {
+        ResultSet rs;
+        ArrayList<Usuario> res = new ArrayList();
+        try 
+        {
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM usuario");    
+            rs = statement.executeQuery();
+            while(rs.next())
+            {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id_u"));
+                usuario.setNickname(rs.getString("nickname"));
+                usuario.setPassword(rs.getString("password"));
+                res.add(usuario);
+            }
+            return res;
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public int insertUser (String nick, String pass)
+    {
+        try 
+        {
+            PreparedStatement statement = con.prepareStatement("INSERT INTO usuario(nickname, password) VALUES ('"+nick+"', '"+pass+"')");
+            return statement.executeUpdate();
+        } 
+        catch (SQLException ex) 
+        {
+            Logger.getLogger(BaseDeDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+}
